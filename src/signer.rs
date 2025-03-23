c273b197ba7fd0ab14aa15f7b6e223c199c02cdc
Naruto Uzumaki <50357815+Naruto-Leader@users.noreply.github.com>
2025-03-23 03:45:47 -0400

    Update signer.rs

--- src/signer.rs
+++ src/signer.rs
@@ -37,7 +37,7 @@ impl Signer for FireblocksSigner {
     }
 
     /// Signs an EIP712 encoded domain separator and message
-    /// TODO: Implement
+    /// TODO: Implement              
     #[allow(unused_variables)]
     async fn sign_typed_data<T: Eip712 + Send + Sync>(
         &self,
